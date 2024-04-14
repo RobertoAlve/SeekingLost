d335691fbca2b08f28f613eeb458d9952fe9a9de
Roberto Alves <roberto_alves59@hotmail.com>
2024-04-14 18:04:41 -0300

    Adicionando componente do botão

--- SeekingLost/src/app/components/button/button.component.ts
+++ SeekingLost/src/app/components/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ButtonColor } from '../../class/ButtonColor';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input() buttonText?: string = "Default";
+  @Input() buttonColor?: string = "#FFF";
+  availableColors: ButtonColor[] = []
+
+  constructor() { }
+
+  ngOnInit() {
+    this.availableColors.push(new ButtonColor("green", "#6CDB7E"))
+    this.availableColors.push(new ButtonColor("red", "#F44E38"))
+    this.availableColors.push(new ButtonColor("purple", "#7B6CDB"))
+
+    this.getButtonColor();
+  }
+
+  private getButtonColor() {
+    this.availableColors.forEach(color => {
+      if (this.buttonColor == color.name) {
+        this.buttonColor = color.value;
+      }
+    })
+  }
 
 }
